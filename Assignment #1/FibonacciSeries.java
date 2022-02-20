e778306c4ddcd3cbc3b112a8e6fec3a2fa5ab09a
Naveen-Work <100067637+Naveen-Work@users.noreply.github.com>
2022-02-20 16:35:34 +0530

    Add files via upload

---
+++ Assignment #1/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int range = 8, firstNum = 0, secNum = 1, sum=0;
+		System.out.print(firstNum);
+		for (int i = 1; i <= range; i++) {
+			sum=firstNum+secNum;
+			System.out.print(" ");
+			System.out.print(sum);
+			firstNum=secNum;
+			secNum=sum;
+		}
+	}	
+
+}
