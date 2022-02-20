b73beb934020f679e556f545e8224ba2ddcd921f
Naveen-Work <100067637+Naveen-Work@users.noreply.github.com>
2022-02-20 16:20:41 +0530

    Add files via upload

---
+++ Assignment #3/ConvertNegativeToPositivNo.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class ConvertNegativeToPositivNo {
+
+	public static void main(String[] args) {
+		int number = -40;
+		int posno=0;
+		if(number<0) {
+			posno=number*-1;
+			System.out.println("The number "+number+" is converted to "+posno);
+		}else {
+			System.out.println("The number "+number+" is Positive number");
+		}
+
+	}
+
+}
