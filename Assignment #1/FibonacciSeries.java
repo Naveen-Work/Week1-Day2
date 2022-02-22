60a98828c1598c80667751ad91f1e08b9386685c
Naveen-Work <100067637+Naveen-Work@users.noreply.github.com>
2022-02-22 14:19:40 +0530

    Update FibonacciSeries.java

--- Assignment #1/FibonacciSeries.java
+++ Assignment #1/FibonacciSeries.java
@@ -5,8 +5,9 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		
 		int range = 8, firstNum = 0, secNum = 1, sum=0;
-		System.out.print(firstNum);
-		for (int i = 1; i <= range; i++) {
+		System.out.print(firstNum+" ");
+		System.out.print(secNum);
+		for (int i = 1; i < range-1; i++) {
 			sum=firstNum+secNum;
 			System.out.print(" ");
 			System.out.print(sum);
